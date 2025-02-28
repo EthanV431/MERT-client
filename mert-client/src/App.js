3c6493dcecca67acd8884c9fd196950e657b1120
Alex Titov <54718346+alextitov19@users.noreply.github.com>
2025-02-28 08:19:23 -0800

    docker and route

--- mert-client/src/App.js
+++ mert-client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import WeatherStation from './Pages/WeatherStation/weatherStation';
-import WeatherStationWindSpeed from './Pages/WeatherStation/WeatherStationGraphs/weatherStationWindSpeed';
-import WeatherStationPressure from './Pages/WeatherStation/WeatherStationGraphs/weatherStationPressure';
-import WeatherStationLightning from './Pages/WeatherStation/WeatherStationGraphs/weatherStationLightning';
-import WeatherStationSolarRadiation from './Pages/WeatherStation/WeatherStationGraphs/weatherStationSolarRadiation';
-import WeatherStationTemperature from './Pages/WeatherStation/WeatherStationGraphs/weatherStationTemperature';
-import WeatherStationPercipitation from './Pages/WeatherStation/WeatherStationGraphs/weatherStationPercipitation';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import WeatherStation from "./Pages/WeatherStation/weatherStation";
+import WeatherStationWindSpeed from "./Pages/WeatherStation/WeatherStationGraphs/weatherStationWindSpeed";
+import WeatherStationPressure from "./Pages/WeatherStation/WeatherStationGraphs/weatherStationPressure";
+import WeatherStationLightning from "./Pages/WeatherStation/WeatherStationGraphs/weatherStationLightning";
+import WeatherStationSolarRadiation from "./Pages/WeatherStation/WeatherStationGraphs/weatherStationSolarRadiation";
+import WeatherStationTemperature from "./Pages/WeatherStation/WeatherStationGraphs/weatherStationTemperature";
+import WeatherStationPercipitation from "./Pages/WeatherStation/WeatherStationGraphs/weatherStationPercipitation";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/mert">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/weatherStation" element={<WeatherStation />} />
-        <Route path="/weatherStationWindSpeed" element={<WeatherStationWindSpeed />} />
-        <Route path="/weatherStationPressure" element={<WeatherStationPressure />} />
-        <Route path="/weatherStationLightning" element={<WeatherStationLightning />} />
-        <Route path="/weatherStationSolarRadiation" element={<WeatherStationSolarRadiation />} />
-        <Route path="/weatherStationTemperature" element={<WeatherStationTemperature />} />
-        <Route path="/weatherStationPercipitation" element={<WeatherStationPercipitation />} />
+        <Route
+          path="/weatherStationWindSpeed"
+          element={<WeatherStationWindSpeed />}
+        />
+        <Route
+          path="/weatherStationPressure"
+          element={<WeatherStationPressure />}
+        />
+        <Route
+          path="/weatherStationLightning"
+          element={<WeatherStationLightning />}
+        />
+        <Route
+          path="/weatherStationSolarRadiation"
+          element={<WeatherStationSolarRadiation />}
+        />
+        <Route
+          path="/weatherStationTemperature"
+          element={<WeatherStationTemperature />}
+        />
+        <Route
+          path="/weatherStationPercipitation"
+          element={<WeatherStationPercipitation />}
+        />
       </Routes>
     </Router>
   );
--- mert-client/upload_new_version.sh
+++ mert-client/upload_new_version.sh
@@ -1,7 +1,7 @@
-docker build --platform linux/amd64 -t mert/client:v0.0.1 .
+docker build --platform linux/amd64 -t mert/client:v0.0.4 .
 
 aws ecr get-login-password --profile personal --region us-east-1 | docker login --username AWS --password-stdin 471112521203.dkr.ecr.us-east-1.amazonaws.com
 
-docker tag mert/client:v0.0.1 471112521203.dkr.ecr.us-east-1.amazonaws.com/mert/client:v0.0.1
+docker tag mert/client:v0.0.4 471112521203.dkr.ecr.us-east-1.amazonaws.com/mert/client:v0.0.4
 
-docker push 471112521203.dkr.ecr.us-east-1.amazonaws.com/mert/client:v0.0.1
+docker push 471112521203.dkr.ecr.us-east-1.amazonaws.com/mert/client:v0.0.4
