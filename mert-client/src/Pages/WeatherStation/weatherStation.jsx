9098d27036aa9c47d34431cf131b6b7021e575f9
Ethan Villarosa <eacvillarosa@gmail.com>
2025-04-15 15:39:19 -0700

    fixed api call

--- mert-client/src/Pages/WeatherStation/weatherStation.jsx
+++ mert-client/src/Pages/WeatherStation/weatherStation.jsx
@@ -34,7 +34,7 @@ const WeatherStation = () => {
     const fromZ = new Date(fromDateTime).toISOString();
     const untilZ = new Date(untilDateTime).toISOString();
 
-    fetch(`/mert/api/data/${fromZ}/${untilZ}`)
+    fetch(`https://alextitov.com/mert/api/data/${fromZ}/${untilZ}`)
       .then(res => res.text())
       .then(rawText => {
         try {
